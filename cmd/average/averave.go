587ec0c54b2d7f01c798d7e2de51e54855aef1c7
vzx7 <coder.vz@gmail.com>
2025-02-20 23:12:16 +0300

    add array

---
+++ cmd/average/averave.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/vzx7/go-head-first/pkg/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GoFloats("cmd/average/data")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	sampleCount := float64(len(numbers))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+}
