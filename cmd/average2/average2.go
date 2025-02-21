497beb63d651012584f87ba279987c5c013f4ef9
vzx7 <coder.vz@gmail.com>
2025-02-21 22:08:30 +0300

    add functions ...args

--- cmd/average2/average2.go
+++ cmd/average2/average2.go
@@ -7,18 +7,25 @@ import (
 	"strconv"
 )
 
+func average(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	arguments := os.Args[1:]
-	var sum float64 = 0
+	var numbers []float64
 
 	for _, argument := range arguments {
 		number, err := strconv.ParseFloat(argument, 64)
 		if err != nil {
 			log.Fatal(err)
 		}
-		sum += number
+		numbers = append(numbers, number)
 	}
 
-	sampleCount := float64(len(arguments))
-	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+	fmt.Printf("Average: %0.2f\n", average(numbers...))
 }
