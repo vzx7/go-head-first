171f281f7f83a3cec1a7786f00ce041ad2daa5ed
vzx7 <coder.vz@gmail.com>
2025-03-05 18:59:25 +0300

    add gorutines

---
+++ cmd/goroutines/gorutines.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Page struct {
+	URL  string
+	Size int
+}
+
+func responseSize(url string, chanel chan Page) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	chanel <- Page{URL: url, Size: len(body)}
+}
+
+func main() {
+	pages := make(chan Page)
+	urls := []string{"https://ya.ru", "https://www.google.com/", "https://www.yahoo.com/"}
+	for _, url := range urls {
+		go responseSize(url, pages)
+	}
+	for i := 0; i < len(urls); i++ {
+		page := <-pages
+		fmt.Printf("%s: %d\n", page.URL, page.Size)
+	}
+}
