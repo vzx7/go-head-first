6e200bd47b018a25e184991ee3411a55bd882f35
Vasiliy Zazdravnykh <coder.vz@gmail.com>
2025-03-13 22:15:27 +0300

    add slices

--- cmd/slices/main.go
+++ cmd/slices/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	b := make([]byte, 12)
@@ -13,4 +16,30 @@ func main() {
 	fmt.Println("Byte slice as text:", string(b))
 
 	fmt.Println("Length of b:", len(b))
+
+	fmt.Println("----------------------------")
+	DoSliceStruct()
+}
+
+type record struct {
+	Field1 int
+	Field2 string
+}
+
+func DoSliceStruct() {
+	S := []record{}
+
+	for i := 0; i < 10; i++ {
+		text := "text" + strconv.Itoa(i)
+		temp := record{Field1: i, Field2: text}
+		S = append(S, temp)
+	}
+
+	fmt.Println("Index 0:", S[0].Field1, S[0].Field2)
+	fmt.Println("Number of structures:", len(S))
+	sum := 0
+	for _, k := range S {
+		sum += k.Field1
+	}
+	fmt.Println("Sum:", sum)
 }
